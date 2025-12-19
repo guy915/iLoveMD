d8012d538ce8fe2a5828fe64ac5f061c5b7b63c8
guy <122138238+guy915@users.noreply.github.com>
2025-12-19 18:54:59 +0200

    feat: add Googlebot-Image rules to robots.txt and update favicon

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,16 @@ import { BASE_URL } from '@/lib/constants'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'Googlebot-Image',
+                allow: '/',
+            },
+        ],
         sitemap: `${BASE_URL}/sitemap.xml`,
     }
 }
